d4353e5c42c6113a44b019b4ed307393a87d4032
dreat <lukskotarek+github@gmail.com>
2016-06-19 21:15:28 +0200

    wip on creating board (phaser game works)

--- draw_things_with_phoenix/web/static/js/socket.js
+++ draw_things_with_phoenix/web/static/js/socket.js
@@ -7,7 +7,7 @@ import {Socket} from "phoenix"
 
 let socket = new Socket("/socket", {params: {token: window.userToken}})
 
-
+const $ = window.$
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
